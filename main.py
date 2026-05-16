affdbd72466735f030abed18513a1aaa8096ffc4
phamminhtri26032014-sketch <phamminhtri26032014@gmail.com>
2026-05-16 18:52:45 +0700

    update ui

--- main.py
+++ main.py
@@ -2,39 +2,34 @@ from flask import Flask, render_template
 from flask_socketio import SocketIO, emit
 
 app = Flask(__name__)
-app.config["SECRET_KEY"] = "chat_secret_2026"
+app.config['SECRET_KEY'] = 'chat_secret'
 
-socketio = SocketIO(app, cors_allowed_origins="*")
+socketio = SocketIO(
+    app,
+    cors_allowed_origins="*",
+    async_mode="eventlet"
+)
 
-online_users = set()
+users = set()
 
 @app.route("/")
 def index():
     return render_template("chat.html")
 
-
 @socketio.on("join")
 def join(data):
     name = data.get("name")
     if name:
-        online_users.add(name)
-    emit("online_users", list(online_users), broadcast=True)
-
+        users.add(name)
+        socketio.emit("online_users", list(users), broadcast=True)
 
 @socketio.on("message_from_client")
-def message(data):
-    emit("message_from_server", data, broadcast=True)
-
-
-@socketio.on("typing")
-def typing(data):
-    emit("typing", data, broadcast=True)
-
+def handle_message(data):
+    socketio.emit("message_from_server", data, broadcast=True)
 
 @socketio.on("disconnect")
 def disconnect():
-    emit("online_users", list(online_users), broadcast=True)
-
+    socketio.emit("online_users", list(users), broadcast=True)
 
 if __name__ == "__main__":
     socketio.run(app, host="0.0.0.0", port=5000)
